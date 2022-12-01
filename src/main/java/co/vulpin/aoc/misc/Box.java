74a03950338569c5393fb14f450ad2cdccf5a528
Nik <nikammerlaan@gmail.com>
2022-12-01 07:40:55 -0800

    Remove unused helper classes

--- src/main/java/co/vulpin/aoc/misc/Box.java
+++
@@ -1,9 +0,0 @@
-package co.vulpin.aoc.misc;
-
-public record Box(Range xRange, Range yRange) {
-
-    public boolean isInBox(Point point) {
-        return xRange.isInRange(point.x()) && yRange.isInRange(point.y());
-    }
-
-}
