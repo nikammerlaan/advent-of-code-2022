fcd36370706e832029d83cccea2036ed87194679
Nik <nikammerlaan@gmail.com>
2022-12-02 21:34:50 -0800

    Add solution for day 3

---
+++ src/test/java/Day03Test.java
@@ -0,0 +1,24 @@
+import co.vulpin.aoc.data.Result;
+import co.vulpin.aoc.days.day03.Day03Solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day03Test extends DayTest {
+
+    public Day03Test() {
+        super(new Day03Solution(), 3);
+    }
+
+    @Override
+    protected void assertExampleResult(Result result) {
+        assertEquals(157, result.part1Result().result());
+        assertEquals(70, result.part2Result().result());
+    }
+
+    @Override
+    protected void assertRealResult(Result result) {
+        assertEquals(7908, result.part1Result().result());
+        assertEquals(2838, result.part2Result().result());
+    }
+
+}
