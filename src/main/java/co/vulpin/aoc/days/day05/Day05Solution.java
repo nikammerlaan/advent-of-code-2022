57a13abb9c07ba067398cfe9c3aca63e534ce7d9
Nik <nikammerlaan@gmail.com>
2022-12-04 21:37:51 -0800

    Add (janky) day 5 solution

---
+++ src/main/java/co/vulpin/aoc/days/day05/Day05Solution.java
@@ -0,0 +1,103 @@
+package co.vulpin.aoc.days.day05;
+
+import co.vulpin.aoc.days.AbstractDaySolution;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day05Solution extends AbstractDaySolution<Day05Solution.Input> {
+
+    @Override
+    protected Object solvePart1(Input input) {
+        var board = input.board().stream()
+            .map(LinkedList::new)
+            .toList();
+
+        for(var instruction : input.instructions()) {
+            var from = board.get(instruction.from() - 1);
+            var to = board.get(instruction.to() - 1);
+
+            for(int i = 0; i < instruction.amount(); i++) {
+                to.push(from.pop());
+            }
+        }
+
+        return getTopCrates(board);
+    }
+
+    @Override
+    protected Object solvePart2(Input input) {
+        var board = input.board().stream()
+            .map(LinkedList::new)
+            .toList();
+
+        for(var instruction : input.instructions()) {
+            var from = board.get(instruction.from() - 1);
+            var to = board.get(instruction.to() - 1);
+
+            for(int i = 0; i < instruction.amount(); i++) {
+                to.push(from.remove(instruction.amount() - i - 1));
+            }
+        }
+
+        return getTopCrates(board);
+    }
+
+    private String getTopCrates(List<LinkedList<Character>> board) {
+        return board.stream()
+            .map(Deque::getFirst)
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+    }
+
+    @Override
+    protected Input parseInput(String rawInput) {
+        var parts = rawInput.split("\n\n");
+
+        var board = parseBoard(parts[0]);
+        var instructions = parseInstructions(parts[1]);
+
+        return new Input(board, instructions);
+    }
+
+    private List<List<Character>> parseBoard(String raw) {
+        var lines = raw.split("\n");
+
+        var columnCount = lines[lines.length - 1].length() / 4 + 1;
+        return IntStream.range(0, columnCount)
+            .mapToObj(i -> {
+                var items = new LinkedList<Character>();
+
+                int charColumn = i * 4 + 1;
+                for(int j = 1; j < lines.length; j++) {
+                    var line = lines[lines.length - j - 1];
+
+                    var c = charColumn < line.length() ? line.charAt(charColumn) : ' ';
+                    if(c != ' ') {
+                        items.push(c);
+                    }
+                }
+
+                return (List<Character>) items;
+            })
+            .toList();
+    }
+
+    private List<Instruction> parseInstructions(String raw) {
+        return raw.lines()
+            .map(s -> {
+                var parts = s.split(" ");
+                return new Instruction(
+                    Integer.parseInt(parts[3]),
+                    Integer.parseInt(parts[5]),
+                    Integer.parseInt(parts[1])
+                );
+            })
+            .toList();
+    }
+    
+    record Input(List<List<Character>> board, List<Instruction> instructions) {}
+    record Instruction(int from, int to, int amount) {}
+
+}
---
+++ src/test/java/Day05Test.java
@@ -0,0 +1,24 @@
+import co.vulpin.aoc.data.Result;
+import co.vulpin.aoc.days.day05.Day05Solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day05Test extends DayTest {
+
+    public Day05Test() {
+        super(new Day05Solution(), 5);
+    }
+
+    @Override
+    protected void assertExampleResult(Result result) {
+        assertEquals("CMZ", result.part1Result().result());
+        assertEquals("MCD", result.part2Result().result());
+    }
+
+    @Override
+    protected void assertRealResult(Result result) {
+        assertEquals("WSFTMRHPP", result.part1Result().result());
+        assertEquals("GSLCMFBRP", result.part2Result().result());
+    }
+
+}
