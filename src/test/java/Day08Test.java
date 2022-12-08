7d2ea0ce9d5ef734402a7e1b96fb27d2bcae44d8
Nik <nikammerlaan@gmail.com>
2022-12-07 21:44:18 -0800

    Add day 8 solution

---
+++ src/test/java/Day08Test.java
@@ -0,0 +1,24 @@
+import co.vulpin.aoc.data.Result;
+import co.vulpin.aoc.days.day08.Day08Solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day08Test extends DayTest {
+
+    public Day08Test() {
+        super(new Day08Solution(), 8);
+    }
+
+    @Override
+    protected void assertExampleResult(Result result) {
+        assertEquals(21, result.part1Result().result());
+        assertEquals(8, result.part2Result().result());
+    }
+
+    @Override
+    protected void assertRealResult(Result result) {
+        assertEquals(1698, result.part1Result().result());
+        assertEquals(672280, result.part2Result().result());
+    }
+
+}
