8dcd5af078471148182d12e61581671fc7fb45b5
Nik <nikammerlaan@gmail.com>
2022-12-10 22:13:40 -0800

    Add day 11 solution

---
+++ src/test/java/Day11Test.java
@@ -0,0 +1,24 @@
+import co.vulpin.aoc.data.Result;
+import co.vulpin.aoc.days.day11.Day11Solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day11Test extends DayTest {
+
+    public Day11Test() {
+        super(new Day11Solution(), 11);
+    }
+
+    @Override
+    protected void assertExampleResult(Result result) {
+        assertEquals(10605L, result.part1Result().result());
+        assertEquals(2713310158L, result.part2Result().result());
+    }
+
+    @Override
+    protected void assertRealResult(Result result) {
+        assertEquals(117624L, result.part1Result().result());
+        assertEquals(16792940265L, result.part2Result().result());
+    }
+
+}
